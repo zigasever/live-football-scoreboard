ed2c4839f5d12cd50c1a8879592ac5dd9d0b931a
Žiga Sever <sever.ziga@gmail.com>
2024-07-18 19:40:35 +0200

    Implemented Team class, added code coverage plugin

--- src/main/java/com/sever/scoreboard/Team.java
+++ src/main/java/com/sever/scoreboard/Team.java
@@ -1,12 +1,28 @@
 package com.sever.scoreboard;
 
+/**
+ * Basic class for match participant.
+ */
 public class Team {
 
-    public Team(String name) {
+    private String name;
 
+    /**
+     * Create a new team with the given name.
+     * @param name the name of the team.
+     */
+    public Team(String name) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Team name must be provided.");
+        }
+        this.name = name;
     }
 
+    /**
+     * Get the name of the team.
+     * @return the name of the team.
+     */
     public String getName() {
-        return null;
+        return name;
     }
 }
