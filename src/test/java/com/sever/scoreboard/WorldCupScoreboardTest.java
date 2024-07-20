234cb40c64e3f899b9872bd13111926eb9930c6f
Žiga Sever <sever.ziga@gmail.com>
2024-07-20 22:30:23 +0200

    Mock matchStart in getMatchSummary_shouldReturnInCorrectOrder, Refactor WorldCupScoreboardTEst

--- src/test/java/com/sever/scoreboard/WorldCupScoreboardTest.java
+++ src/test/java/com/sever/scoreboard/WorldCupScoreboardTest.java
@@ -1,14 +1,17 @@
 package com.sever.scoreboard;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class WorldCupScoreboardTest {
+class WorldCupScoreboardTest {
 
     private WorldCupScoreBoard createScoreboard() {
         return new WorldCupScoreBoard();
@@ -62,23 +65,38 @@ public class WorldCupScoreboardTest {
         assertEquals(null, scoreboard.getMatch(matchId));
     }
 
+    private void mockStartTime(TeamSportMatch match, int delay) {
+        Field matchStartField = ReflectionUtils
+                .findFields(FootballMatch.class, f -> f.getName().equals("matchStart"),
+                        ReflectionUtils.HierarchyTraversalMode.BOTTOM_UP)
+                .get(0);
+        matchStartField.setAccessible(true);
+        try {
+            matchStartField.set(match, LocalDateTime.now().plusMinutes(delay));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
-    void getMatchSummary_shouldReturnInCorrectOrder() throws InterruptedException{
+    void getMatchSummary_shouldReturnInCorrectOrder() {
         WorldCupScoreBoard scoreboard = createScoreboard();
+        int delayMinute = 0;
         TeamSportMatch match1 = scoreboard.startMatch(1, new Team("Mexico"), new Team("Canada"));
         match1.setScore(0, 5);
-        Thread.sleep(1);
+        mockStartTime(match1, delayMinute++);
         TeamSportMatch match2 = scoreboard.startMatch(2, new Team("Spain"), new Team("Brazil"));
         match2.setScore(10, 2);
-        Thread.sleep(1);
+        mockStartTime(match2, delayMinute++);
         TeamSportMatch match3 = scoreboard.startMatch(3, new Team("Germany"), new Team("France"));
         match3.setScore(2, 2);
-        Thread.sleep(1);
+        mockStartTime(match3, delayMinute++);
         TeamSportMatch match4 = scoreboard.startMatch(4, new Team("Uruguay"), new Team("Italy"));
         match4.setScore(6, 6);
-        Thread.sleep(1);
+        mockStartTime(match4, delayMinute++);
         TeamSportMatch match5 = scoreboard.startMatch(5, new Team("Argentina"), new Team("Australia"));
         match5.setScore(3, 1);
+        mockStartTime(match5, delayMinute);
 
         final Collection correctOrder = List.of(match4, match2, match1, match5, match3);
 
@@ -88,11 +106,14 @@ public class WorldCupScoreboardTest {
     @Test
     void teamShouldNotPlayMultipleMatchesAtOnce () {
         WorldCupScoreBoard scoreboard = createScoreboard();
-        TeamSportMatch match1 = scoreboard.startMatch(1, new Team("Mexico"), new Team("Canada"));
+        scoreboard.startMatch(1, new Team("Mexico"), new Team("Canada"));
+
+        Team t1 = new Team("Mexico");
+        Team t2 = new Team("Brazil");
 
         IllegalArgumentException e = assertThrows(
                 IllegalArgumentException.class,
-                () -> scoreboard.startMatch(2, new Team("Mexico"), new Team("Brazil")),
+                () -> scoreboard.startMatch(2, t1, t2),
                 "Expected startMatch() to throw IllegalArgumentException, but nothing was thrown.");
 
         assertEquals("One of the teams is already participating in another match.", e.getMessage());
@@ -102,11 +123,14 @@ public class WorldCupScoreboardTest {
     @Test
     void matchesShouldHaveUniqueIds() {
         WorldCupScoreBoard scoreboard = createScoreboard();
-        TeamSportMatch match1 = scoreboard.startMatch(1, new Team("Mexico"), new Team("Canada"));
+        scoreboard.startMatch(1, new Team("Mexico"), new Team("Canada"));
+
+        Team t1 = new Team("Brazil");
+        Team t2 = new Team("Argentina");
 
         IllegalArgumentException e = assertThrows(
                 IllegalArgumentException.class,
-                () -> scoreboard.startMatch(1, new Team("Brazil"), new Team("Argentina")),
+                () -> scoreboard.startMatch(1, t1, t2),
                 "Expected startMatch() to throw IllegalArgumentException, but nothing was thrown.");
 
         assertEquals("Match with id 1 already exists.", e.getMessage());
