dc1cab7db732b3b7257cb3ac0d91cabd8dd7b02c
Žiga Sever <sever.ziga@gmail.com>
2024-07-20 18:23:04 +0200

    FootballMatch implementation

--- src/main/java/com/sever/scoreboard/FootballMatch.java
+++ src/main/java/com/sever/scoreboard/FootballMatch.java
@@ -4,42 +4,59 @@ import java.time.LocalDateTime;
 
 public class FootballMatch implements TeamSportMatch {
 
-    public FootballMatch(Team homeTeam, Team awayTeam) {
+    private final Team homeTeam;
+    private final Team awayTeam;
+    private int homeScore = 0;
+    private int awayScore = 0;
+    private final LocalDateTime matchStart = LocalDateTime.now();
 
+    public FootballMatch(Team homeTeam, Team awayTeam) {
+        if(awayTeam == null || homeTeam == null){
+            throw new IllegalArgumentException("Teams must be provided");
+        }
+        if(awayTeam.equals(homeTeam)){
+            throw new IllegalArgumentException("Teams must be different");
+        }
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
     }
 
     @Override
     public Team getHomeTeam() {
-        return null;
+        return homeTeam;
     }
 
     @Override
     public Team getAwayTeam() {
-        return null;
+        return awayTeam;
     }
 
     @Override
     public int getHomeScore() {
-        return 0;
+        return homeScore;
     }
 
     @Override
     public int getAwayScore() {
-        return 0;
+        return awayScore;
     }
 
     @Override
     public void setScore(int homeScore, int awayScore) {
-
+        if(homeScore < 0 || awayScore < 0) {
+            throw new IllegalArgumentException("Score cannot be updated to a negative number");
+        }
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
     }
 
     @Override
     public int getTotalScore() {
-        return 0;
+        return homeScore + awayScore;
     }
 
     @Override
     public LocalDateTime getMatchStart() {
-        return null;
+        return matchStart;
     }
 }
